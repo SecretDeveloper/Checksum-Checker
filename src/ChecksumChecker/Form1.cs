fb6f0e88101b6489cea637bd98cf087e19bcc4d8
Gary Kenneally <garykenneally@gmail.com>
2012-02-24 16:22:42 +0000

    Squashed commit of the following:Added support for SHA1 SHA256 and MD5. installer still required
    commit 894f104f2a629ffc1d1ed87e40615b1d182df641
    Author: Gary Kenneally <garykenneally@gmail.com>
    Date:   Fri Feb 24 15:43:53 2012 +0000
    
        irst version.

---
+++ src/ChecksumChecker/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ChecksumLib;
+
+namespace ChecksumChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            try
+            {                
+                if (openFileDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                    return;
+
+                string filePath = openFileDialog.FileName;
+                RunHash(filePath, GetChecksumTypeFromString(this.comboBox1.SelectedItem.ToString()));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private ChecksumType GetChecksumTypeFromString(string typeString)
+        {
+            switch (typeString.ToUpper())
+            {
+                case "SHA1":
+                    return ChecksumType.SHA1;
+                case "SHA256":
+                    return ChecksumType.SHA256;
+                case "MD5":
+                    return ChecksumType.MD5;
+            }
+            return ChecksumType.SHA1;
+        }
+
+        private void RunHash(string filePath, ChecksumType type)
+        {
+            this.lblResult.Text = "";
+            this.txtSource.Text = filePath;
+
+            Checker ch = new Checker();
+            this.txtChecksum.Text = ch.GetChecksum(filePath, type);
+
+            if (txtCompare.Text.Length > 0)
+                lblResult.Text = CompareHashes(txtChecksum.Text, txtCompare.Text);
+        }
+
+        private void txtCompare_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txtCompare.Text.Length >0)
+                    lblResult.Text = CompareHashes(txtChecksum.Text, txtCompare.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string CompareHashes(string generated, string pasted)
+        {
+            if (generated.Trim().Replace("-", "").Equals(pasted.Trim().Replace("-", ""), StringComparison.InvariantCultureIgnoreCase))
+                return "Hashes match";
+
+            return "Hashes do not match";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = openFileDialog.FileName;
+                
+                RunHash(filePath, GetChecksumTypeFromString(this.comboBox1.SelectedItem.ToString()));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ src/ChecksumLib/Checker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace ChecksumLib
+{
+    public class Checker
+    {
+
+        public string GetChecksum(string filePath, ChecksumType type)
+        {
+            if(!File.Exists(filePath))
+                return "";
+            string hash = "";
+            using(var fs = new FileStream(filePath, FileMode.Open))
+            {
+                using (var cryptoProvider = GetCryptoProvider(type))
+                {
+                    hash = BitConverter.ToString(cryptoProvider.ComputeHash(fs));                
+                }
+            }
+
+            return hash;
+        }
+
+        private HashAlgorithm GetCryptoProvider(ChecksumType type)
+        {
+            switch (type)
+            {
+                case ChecksumType.SHA1:
+                    return new SHA1Managed();                    
+
+                case ChecksumType.SHA256:
+                    return new SHA256Managed();                    
+
+                case ChecksumType.MD5:
+                    return new MD5CryptoServiceProvider();                    
+
+                default:
+                    throw new ApplicationException("Unsupported Checksum type");
+            }
+        }
+
+    }
+}
